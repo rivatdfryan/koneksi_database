cce4f43759dbd17635b02409d70d4f8cd86e73ff
RyzenXRS <rioenjoy7@gmail.com>
2025-04-22 10:42:13 +0700

    jfjds

---
+++ update_db.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+public class update_db
+{
+	public update()
+	{
+        string connectionString = "Host=localhost;Port=5432;Username=postgres;Password=a.1056.A;Database=PBO;";
+        NpgsqlConnection connection = new NpgsqlConnection(connectionString);
+        string updateSql = "UPDATE jualan SET stok_barang = @stok_barang WHERE id_barang = @id_barang";
+        NpgsqlCommand updateCommand = new NpgsqlCommand(updateSql, connection);
+
+        Console.Write("Masukkan Stok Barang : ")
+        updateCommand.Parameters.AddWithValue("@stok_barang", 700);
+
+        Console.Write("Masukkan ID Barang : ")
+        updateCommand.Parameters.AddWithValue("@id_barang", "1");
+
+        try
+        {
+            connection.Open();
+            int rowsAffected = updateCommand.ExecuteNonQuery();
+            Console.WriteLine($"Updated {rowsAffected} row(s)!");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+        finally
+        {
+            connection.Close();
+        }
+
+    }
+}
